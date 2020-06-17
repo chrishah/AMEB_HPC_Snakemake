8788008345c65ae6d312f131f3897022a279926c
chrishah <chrisi.hahni@gmail.com>
2020-06-17 09:42:12 +0200

    add data

---
+++ data/submission.template.sge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#$ -N test	   	# Job name
+#$ -S /bin/bash         # Set shell to bash
+#
+#$ -l h_vmem=2G         # Request Max. Virt. Mem.
+#
+#$ -q all.q		# choose the queue
+#$ -cwd                 # Change to current working directory
+#$ -V                   # Export environment variables into script
+#$ -pe smp 1    	# Select the parallel environment
+#
+#$ -o log.$JOB_NAME.$JOB_ID.out      # SGE-Output File
+#$ -e log.$JOB_NAME.$JOB_ID.err      # SGE-Error File
+
+#print some info to log
+echo "Running under shell '${SHELL}' in directory '`pwd`' using $NSLOTS slots"
+echo "Host: $HOSTNAME"
+echo "Job: $JOB_ID"
+
+#get going
+
+
